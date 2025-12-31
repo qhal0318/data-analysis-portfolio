3b4dc0305b31739971920d525e9696a4ae94d611
qhal0318 <qhal0318@gmail.com>
2025-12-31 15:50:39 +0900

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+   st.set_page_config(
+       page_title="지연주 데이터 분석 포트폴리오",
+       page_icon="📊",
+       layout="wide"
+   )
+
+   st.title("📊 데이터 분석 포트폴리오")
+   st.markdown("### 🚀 준비 중입니다...")
